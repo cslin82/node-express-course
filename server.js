78ba6dd984413d99dd1b473857b8fa76eb784f90
Chris Lin <35543509+cslin82@users.noreply.github.com>
2020-05-26 07:25:43 -0400

    Format

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-const mockUserData=[
-	{name:'Mark'},
-	{name:'Jill'}
+const mockUserData = [
+	{ name: 'Mark' },
+	{ name: 'Jill' }
 ]
 
-app.get('/users',function(req,res){
+app.get('/users', function (req, res) {
 	res.json({
 		success: true,
 		message: 'successfully got users. Nice!',
@@ -17,7 +17,7 @@ app.get('/users',function(req,res){
 	})
 })
 
-app.get('/users/:id',function(req,res){
+app.get('/users/:id', function (req, res) {
 	console.log(req.params.id)
 	res.json({
 		success: true,
@@ -26,25 +26,25 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
-app.post('/login',function(req,res){
-	const username=req.body.username;
-	const password=req.body.password;
+app.post('/login', function (req, res) {
+	const username = req.body.username;
+	const password = req.body.password;
 
-	const mockUsername="billyTheKid";
-	const mockPassword="superSecret";
+	const mockUsername = "billyTheKid";
+	const mockPassword = "superSecret";
 
-	if (username===mockUsername && password===mockPassword){
-		 res.json({
-			  success: true,
-			  message: 'password and username match!',
-			  token: 'encrypted token goes here'
-		 })
+	if (username === mockUsername && password === mockPassword) {
+		res.json({
+			success: true,
+			message: 'password and username match!',
+			token: 'encrypted token goes here'
+		})
 	} else {
-		 res.json({
-			  success: false,
-			  message: 'password and username do not match'
-		 })
+		res.json({
+			success: false,
+			message: 'password and username do not match'
+		})
 	}
 })
 
-app.listen(8000,function(){console.log('server is listening')})
+app.listen(8000, function () { console.log('server is listening') })
